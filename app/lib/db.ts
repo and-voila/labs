52b3f81d5ff6c57acf0a036e84c66154a371d47e
Sam Rizvi <srizvi@users.noreply.github.com>
2023-11-14 22:18:23 -0800

    chore: cast types for accelerate client
    - extended client type to address build issues

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -2,7 +2,9 @@ import { PrismaClient } from '@prisma/client';
 import { withAccelerate } from '@prisma/extension-accelerate';
 
 const prismaClientSingleton = () => {
-  return new PrismaClient().$extends(withAccelerate());
+  const baseClient = new PrismaClient();
+  const extendedClient = baseClient.$extends(withAccelerate());
+  return extendedClient as PrismaClient & typeof extendedClient;
 };
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
