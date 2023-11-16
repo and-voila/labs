dee0bade3202d56d06f37d73264a3350250594a1
Sam Rizvi <srizvi@users.noreply.github.com>
2023-11-16 02:03:12 -0800

    chore: update auth provider profile returns #106
    - mapped discord provider fields to custom schema
    - also added auth to insights and guides
    
    Closes #106 and #67

--- app/(routes)/(insights)/layout.tsx
+++ app/(routes)/(insights)/layout.tsx
@@ -1,9 +1,12 @@
+import { redirect } from 'next/navigation';
+
 import { InsightsSearch } from '@/app/components/insights/search';
 import { InsightsSidebarNav } from '@/app/components/insights/sidebar-nav';
 import { NavBar } from '@/app/components/layout/navbar';
 import { SiteFooter } from '@/app/components/layout/site-footer';
 import { Icons } from '@/app/components/shared/icons';
 import { insightsConfig } from '@/app/config/insights';
+import { authOptions } from '@/app/lib/auth';
 import { getCurrentUser } from '@/app/lib/session';
 
 interface InsightsLayoutProps {
@@ -25,6 +28,10 @@ export default async function InsightsLayout({
   children,
 }: InsightsLayoutProps) {
   const user = await getCurrentUser();
+
+  if (!user) {
+    redirect(authOptions?.pages?.signIn || '/login');
+  }
 
   return (
     <div className="flex min-h-screen flex-col">
