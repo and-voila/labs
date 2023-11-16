c51f0e3f99043844073685dbbc582d438fc28562
Sam Rizvi <srizvi@users.noreply.github.com>
2023-11-16 01:30:44 -0800

    style: reduced size of pre in mdx

--- app/components/content/mdx-components.tsx
+++ app/components/content/mdx-components.tsx
@@ -163,7 +163,7 @@ const components = {
   code: ({ className, ...props }: MDXComponentProps) => (
     <code
       className={cn(
-        'relative rounded border px-[0.3rem] py-[0.2rem] font-mono text-sm md:text-base lg:text-lg',
+        'relative rounded border px-[0.3rem] py-[0.2rem] font-mono text-sm md:text-base',
         className,
       )}
       {...props}
