59389c2b91a08637bbfe80c2fb8b9d18d0d8ba6e
Sam Rizvi <srizvi@users.noreply.github.com>
2023-11-18 19:47:30 -0800

    chore: migrate and merge write structure #110
    - updated middleware
    
    Closes #110

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,29 @@ import { withAuth } from 'next-auth/middleware';
 
 export default withAuth(
   async function middleware(req) {
+    let hostname = req.headers.get('host') || '';
+
+    if (hostname.endsWith('.localhost:3001')) {
+      hostname = hostname.replace(
+        '.localhost:3001',
+        `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`,
+      );
+    } else if (
+      hostname.includes('---') &&
+      hostname.endsWith(`.${process.env.NEXT_PUBLIC_VERCEL_DEPLOYMENT_SUFFIX}`)
+    ) {
+      hostname = `${hostname.split('---')[0]}.${
+        process.env.NEXT_PUBLIC_ROOT_DOMAIN
+      }`;
+    }
+
+    const mainAppDomain = `labs.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`;
+    const localAppDomain = 'localhost:3001';
+
+    if (hostname !== localAppDomain && hostname !== mainAppDomain) {
+      return NextResponse.next();
+    }
+
     const userAgent = req.headers.get('user-agent') || '';
     const isCrawler =
       /facebookexternalhit|baiduspider|bingbot|discordbot|duckduckbot|googlebot|linkedinbot|pinterestbot|slackbot|tiktokbot|twitterbot|yandexbot/i.test(
@@ -25,7 +48,6 @@ export default withAuth(
         const from = req.nextUrl.searchParams.get('from') || '/dashboard';
         return NextResponse.redirect(new URL(from, req.url));
       }
-
       return null;
     }
 
@@ -34,11 +56,26 @@ export default withAuth(
       if (req.nextUrl.search) {
         from += req.nextUrl.search;
       }
-
       return NextResponse.redirect(
         new URL(`/login?from=${encodeURIComponent(from)}`, req.url),
       );
     }
+
+    if (hostname !== localAppDomain && hostname !== mainAppDomain) {
+      if (!hostname.startsWith('app.') && !hostname.includes('labs')) {
+        const path = req.nextUrl.pathname;
+        const searchParams = req.nextUrl.searchParams.toString();
+        const fullPath = `${path}${
+          searchParams.length > 0 ? `?${searchParams}` : ''
+        }`;
+
+        return NextResponse.rewrite(
+          new URL(`/${hostname}${fullPath}`, req.url),
+        );
+      }
+    }
+
+    return NextResponse.next();
   },
   {
     callbacks: {
