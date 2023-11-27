460010891efbc22923d1b5f07a69a2a93cc9fc0e
Sam Rizvi <srizvi@users.noreply.github.com>
2023-11-27 06:46:27 -0800

    feat: add react memo to post title and description

--- app/components/write/editor/post-description-input.tsx
+++ app/components/write/editor/post-description-input.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import TextareaAutosize from 'react-textarea-autosize';
 
 interface PostDescriptionInputProps {
@@ -5,21 +6,21 @@ interface PostDescriptionInputProps {
   onChange: (value: string) => void;
 }
 
-const PostDescriptionInput = ({
-  value,
-  onChange,
-}: PostDescriptionInputProps) => {
-  return (
-    <TextareaAutosize
-      placeholder="Your super descriptive SEO description goes here. It should include keyword(s) for the post and provide a good summary for search. 157 characters max please."
-      minLength={100}
-      maxLength={159}
-      required
-      defaultValue={value}
-      onChange={(e) => onChange(e.target.value)}
-      className="w-[5/6 resize-none border-none bg-card px-0 font-sans placeholder:text-muted-foreground/70 focus:outline-none focus:ring-0"
-    />
-  );
-};
+const PostDescriptionInput = React.memo(
+  ({ value, onChange }: PostDescriptionInputProps) => {
+    return (
+      <TextareaAutosize
+        placeholder="Your super descriptive SEO description goes here. It should include keyword(s) for the post and provide a good summary for search. 157 characters max please."
+        minLength={100}
+        maxLength={159}
+        required
+        defaultValue={value}
+        onChange={(e) => onChange(e.target.value)}
+        className="w-[5/6 resize-none border-none bg-card px-0 font-sans placeholder:text-muted-foreground/70 focus:outline-none focus:ring-0"
+      />
+    );
+  },
+);
+PostDescriptionInput.displayName = 'PostDescriptionInput';
 
 export default PostDescriptionInput;
