3c3030fa67c030e0e54c71818f9b2d942bdbe413
Sam Rizvi <srizvi@users.noreply.github.com>
2023-11-27 03:43:26 -0800

    feat: create component for post description

---
+++ app/components/write/editor/post-description-input.tsx
@@ -0,0 +1,25 @@
+import TextareaAutosize from 'react-textarea-autosize';
+
+interface PostDescriptionInputProps {
+  value: string;
+  onChange: (value: string) => void;
+}
+
+const PostDescriptionInput = ({
+  value,
+  onChange,
+}: PostDescriptionInputProps) => {
+  return (
+    <TextareaAutosize
+      placeholder="Your super descriptive SEO description goes here. It should include keyword(s) for the post and provide a good summary for search. 157 characters max please."
+      minLength={100}
+      maxLength={159}
+      required
+      defaultValue={value}
+      onChange={(e) => onChange(e.target.value)}
+      className="w-[5/6 resize-none border-none bg-card px-0 font-sans placeholder:text-muted-foreground/70 focus:outline-none focus:ring-0"
+    />
+  );
+};
+
+export default PostDescriptionInput;
