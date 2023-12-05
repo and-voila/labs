fd8672f10a63ee88daaefa9aa42d87dab9a6b092
Sam Rizvi <srizvi@users.noreply.github.com>
2023-12-04 20:26:32 -0800

    feat: add top loader #323
    - added top loader package
      - included a workaround for known issue
      - added nprogress for workaround
      - will be addressed in next nextjs update
    
    Closes #323

--- app/components/providers/providers.tsx
+++ app/components/providers/providers.tsx
@@ -1,7 +1,10 @@
 'use client';
 
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
+import { usePathname, useRouter } from 'next/navigation';
 import { SessionProvider } from 'next-auth/react';
+import NextTopLoader from 'nextjs-toploader';
+import nProgress from 'nprogress';
 import { Provider as BalancerProvider } from 'react-wrap-balancer';
 
 import { ConfettiProvider } from '@/app/components/providers/confetti-provider';
@@ -10,10 +13,32 @@ import { Toaster } from '@/app/components/ui/toaster';
 import { ModalProvider } from '@/app/components/write/modal/provider';
 
 export function Providers({ children }: { children: ReactNode }) {
+  // TODO: Remove this upon next Next JS package update see: https://github.com/TheSGJ/nextjs-toploader/issues/56#issuecomment-1833133639
+  const pathname = usePathname();
+  const router = useRouter();
+  useEffect(() => {
+    nProgress.done();
+  }, [pathname, router]);
+
   return (
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
       <SessionProvider>
-        <ModalProvider>{children}</ModalProvider>
+        <ModalProvider>
+          <NextTopLoader
+            color="#6032ec"
+            initialPosition={0.08}
+            crawlSpeed={20}
+            height={3}
+            crawl={true}
+            showSpinner={false}
+            easing="ease"
+            speed={20}
+            shadow="0 0 10px #6032ec,0 0 5px #6032ec"
+            zIndex={1600}
+            showAtBottom={false}
+          />
+          {children}
+        </ModalProvider>
       </SessionProvider>
 
       <ConfettiProvider />
