6257b04c78bfc812438d3f6f7d27503c06a5e41a
Sam Rizvi <srizvi@users.noreply.github.com>
2023-12-15 20:12:18 -0800

    fix: not authorized styling #438
    - updated not-authorized
    - updated oops
    - updated error
    - updated not found
    
    Closes #438

--- src/app/(routes)/(marketing)/error.tsx
+++ src/app/(routes)/(marketing)/error.tsx
@@ -1,14 +1,88 @@
 'use client';
 
+import { CP_PREFIX } from '#/lib/const';
+import { IconKey } from '#/lib/types';
+
+import { Icons } from '#/components/shared/icons';
 import { Button } from '#/components/ui/button';
+
+const links: Array<{
+  name: string;
+  href: string;
+  description: string;
+  icon: IconKey;
+}> = [
+  {
+    name: 'Dashboard',
+    href: `${CP_PREFIX}/settings/workspaces`,
+    description:
+      'Your command center for everything And Voila. Navigate like a pro.',
+    icon: 'dashboard',
+  },
+  {
+    name: 'Live support',
+    href: `${CP_PREFIX}/settings/support`,
+    description:
+      'Stuck? Chat with our support folks live and get unstuck in a jiffy.',
+    icon: 'discord',
+  },
+];
 
 export default function Error({ reset }: { reset: () => void }) {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
-      <h2 className="mb-5 text-center">Something went wrong!</h2>
-      <Button type="submit" variant="default" onClick={() => reset()}>
-        Try again
-      </Button>
-    </div>
+    <main className="mx-auto w-full max-w-7xl px-6 pb-16 pt-10 sm:pb-24 lg:px-8">
+      <div className="mx-auto mt-20 max-w-2xl text-center sm:mt-24">
+        <p className="text-base font-medium uppercase leading-8 tracking-widest text-primary">
+          Error
+        </p>
+        <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
+          Whoops, tech hiccup
+        </h1>
+        <p className="mt-4 text-base leading-7 text-muted-foreground sm:mt-6 sm:text-lg sm:leading-8">
+          Looks like our tech just tripped over its own feet. We&apos;re on it,
+          so hang tight.
+        </p>
+      </div>
+      <div className="mx-auto mt-16 flow-root max-w-lg sm:mt-20">
+        <h2 className="sr-only">Popular pages</h2>
+        <ul
+          role="list"
+          className="-mt-6 divide-y divide-border border-b border-border"
+        >
+          {links.map((link, linkIdx) => {
+            const Icon = Icons[link.icon];
+            return (
+              <li key={linkIdx} className="relative flex gap-x-6 py-6">
+                <div className="flex h-10 w-10 flex-none items-center justify-center rounded-lg shadow-sm">
+                  <Icon className="h-6 w-6 text-primary" aria-hidden="true" />
+                </div>
+                <div className="flex-auto">
+                  <h3 className="text-sm font-semibold leading-6">
+                    <a href={link.href}>
+                      <span className="absolute inset-0" aria-hidden="true" />
+                      {link.name}
+                    </a>
+                  </h3>
+                  <p className="mt-2 leading-6 text-muted-foreground">
+                    {link.description}
+                  </p>
+                </div>
+                <div className="flex-none self-center">
+                  <Icons.radixChevronRight
+                    className="h-5 w-5 text-primary"
+                    aria-hidden="true"
+                  />
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+        <div className="mt-10 flex justify-center">
+          <Button type="submit" variant="default" onClick={() => reset()}>
+            Try again
+          </Button>
+        </div>
+      </div>
+    </main>
   );
 }
--- src/app/(routes)/app/(teams)/[team_slug]/not-found.tsx
+++ src/app/(routes)/app/(teams)/[team_slug]/not-found.tsx
@@ -1,15 +1,95 @@
+import { Metadata } from 'next';
 import Link from 'next/link';
 
 import { CP_PREFIX } from '#/lib/const';
+import { cn } from '#/lib/utils';
+
+import { DashboardShell } from '#/components/dashboard/shell';
+import { buttonVariants } from '#/components/ui/button';
 
 const TeamNotFound = () => {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href={CP_PREFIX}>Return Home</Link>
-    </div>
+    <DashboardShell>
+      <main className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <p className="text-base font-medium uppercase tracking-widest text-primary">
+            404
+          </p>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
+            Page not found
+          </h1>
+          <p className="mt-6 max-w-xl text-base leading-7 text-muted-foreground">
+            Looks like this page took a personal day. Sorry, we couldn&apos;t
+            find it for you.
+          </p>
+
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <Link
+              href={`${CP_PREFIX}/settings/workspaces`}
+              className={cn(buttonVariants())}
+            >
+              Dashboard
+            </Link>
+            <Link
+              href={`${CP_PREFIX}/settings/support`}
+              className="text-sm font-semibold text-muted-foreground hover:text-muted-foreground/80"
+            >
+              Contact support <span aria-hidden="true">&rarr;</span>
+            </Link>
+          </div>
+        </div>
+      </main>
+    </DashboardShell>
   );
 };
 
 export default TeamNotFound;
+
+export function generateMetadata(): Metadata {
+  const title = '404 Not Found';
+  const description =
+    "Oops! Even our links get lost sometimes. This And Voila page has vanished, but your digital marketing journey hasn't.";
+
+  const baseUrl =
+    process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview'
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+      : process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3001';
+
+  const ogImageUrl = new URL(`${baseUrl}/api/og`);
+  ogImageUrl.searchParams.set('title', title);
+
+  const pageUrl = `${baseUrl}${CP_PREFIX}/settings/advanced`;
+
+  const metadata = {
+    title,
+    description,
+    openGraph: {
+      type: 'website',
+      title,
+      description,
+      images: [
+        {
+          url: ogImageUrl.toString(),
+          width: 1200,
+          height: 630,
+          alt: title,
+        },
+      ],
+      url: pageUrl,
+    },
+    twitter: {
+      title,
+      description,
+      images: [
+        {
+          url: ogImageUrl.toString(),
+          width: 1200,
+          height: 630,
+          alt: title,
+        },
+      ],
+    },
+  };
+
+  return metadata;
+}
