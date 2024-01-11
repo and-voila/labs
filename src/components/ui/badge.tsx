7d21be60228e3e237483e4062fc76315491f4b7f
Sam Rizvi <srizvi@users.noreply.github.com>
2024-01-11 01:33:54 -0800

    style: update palette

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -4,21 +4,21 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '#/lib/utils';
 
 const badgeVariants = cva(
-  'inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
+  'inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 shadow-sm',
   {
     variants: {
       variant: {
         default:
-          'border-transparent bg-primary text-primary-foreground shadow hover:bg-primary/80',
+          'border-transparent bg-primary text-primary-foreground hover:bg-primary/80',
         secondary:
           'border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80',
         destructive:
-          'border-transparent bg-destructive text-destructive-foreground shadow hover:bg-destructive/80',
+          'border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80',
         outline: 'text-foreground',
         custom:
-          'border-alternate bg-transparent text-alternate shadow pointer-events-none',
+          'border-alternate bg-transparent text-alternate pointer-events-none',
         comingSoon:
-          'border-alternate bg-transparent text-alternate text-[10px] px-1 shadow pointer-events-none ml-2',
+          'border-alternate bg-transparent text-alternate text-[10px] px-1 pointer-events-none ml-2',
       },
     },
     defaultVariants: {
