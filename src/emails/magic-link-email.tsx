d7bb0f35206e961d6d856f1b06b320872f8e0f6a
Sam Rizvi <srizvi@users.noreply.github.com>
2024-01-12 20:20:10 -0800

    feat: create emails and revise #449
    - updated colors
    - tweaked some copy
    
    Closes #449

--- src/emails/magic-link-email.tsx
+++ src/emails/magic-link-email.tsx
@@ -26,12 +26,10 @@ export const MagicLinkEmail = ({
 }: MagicLinkEmailProps) => (
   <Html>
     <Head />
-    <Preview>
-      Step into the world of smarter digital marketing with And Voila.
-    </Preview>
+    <Preview>Here&apos;s the magic link you requested from And Voila.</Preview>
     <Tailwind>
-      <Body className="bg-[#dcdfe5] py-[24px] font-sans">
-        <Container className="mx-auto rounded-lg bg-[#ffffff] px-[24px] pt-[36px]">
+      <Body className="bg-[#B4B4B4] py-[24px] font-sans">
+        <Container className="mx-auto rounded-lg bg-[#F8F9FB] px-[24px] pt-[36px]">
           <Img
             src={`${SITE_URL}/and-voila-logo.png`}
             width="150"
@@ -39,31 +37,36 @@ export const MagicLinkEmail = ({
             alt="A colored logomark with an ampersand and exclamation icon and the the Voila"
             className="mx-auto justify-center"
           />
-          <Hr className="border-t-1 mt-[16px] border-[#a18ef7]" />
-          <Text className="mt-[40px] text-[22px] leading-[28px] text-[#61636b]">
+          <Hr className="border-t-1 mt-[16px] border-[#d20f9a]" />
+          <Text className="mt-[40px] text-[22px] leading-[28px] text-[#6E6E6E]">
             🌺 Aloha,
           </Text>
-          <Text className="text-[22px] leading-[28px] text-[#61636b]">
-            We&apos;re excited to have you join us. Just one more click and
-            you&apos;re in.{' '}
-            {mailType === 'login' ? 'Log in' : 'Activate your account'} with the
-            button below.
-          </Text>
+          {mailType === 'login' ? (
+            <Text className="text-[22px] leading-[28px] text-[#6E6E6E]">
+              We&apos;re excited to have you back. Click the button below to log
+              in.
+            </Text>
+          ) : (
+            <Text className="text-[22px] leading-[28px] text-[#6E6E6E]">
+              We&apos;re thrilled to welcome you aboard. Just one more click and
+              you&apos;re in. Activate your account with the button below.
+            </Text>
+          )}
           <Section className="mt-[48px] text-center">
             <Button
-              className="inline-block w-[85%] rounded-md bg-[#6032ec] px-[32px] py-[16px] text-[22px] leading-[28px] text-[#ffffff] no-underline"
+              className="inline-block w-[85%] rounded-md bg-[#18A300] px-[32px] py-[16px] text-[22px] leading-[28px] text-[#F8F9FB] no-underline"
               href={actionUrl}
             >
               {mailType === 'login' ? 'Log In' : 'Activate Account'}
             </Button>
           </Section>
-          <Text className="text-center text-[12px] text-[#61636b]">
-            Expires in 24 hours and is a one-time pass.
+          <Text className="text-center text-[12px] text-[#6E6E6E]">
+            This link will expire in 24 hours and is for one-time use only.
           </Text>
-          <Hr className="border-t-1 mt-[48px] border-[#a18ef7]" />
-          <Text className="mt-[40px] text-[12px] text-[#61636b]">
-            We sent you this email because you asked for a magic link. If it
-            wasn&apos;t you, please ignore it.
+          <Hr className="border-t-1 mt-[48px] border-[#d20f9a]" />
+          <Text className="mt-[40px] text-[12px] text-[#6E6E6E]">
+            If you didn&apos;t request this magic link, feel free to disregard
+            this email.
           </Text>
           <Text className="text-[12px]">
             <span className="font-bold">BRIL.LA, LLC.</span> 1370 N. St. Andrews
