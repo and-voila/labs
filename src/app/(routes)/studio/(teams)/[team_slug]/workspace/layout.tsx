96a3b0efda632cdc6beb88dc192fee480bd8409b
Sam Rizvi <srizvi@users.noreply.github.com>
2024-01-14 16:44:30 -0800

    style: mobile not supported #576
    - added mobile not supported to dashboard routes
    
    Closes #576, closes #455

--- src/app/(routes)/studio/(teams)/[team_slug]/workspace/layout.tsx
+++ src/app/(routes)/studio/(teams)/[team_slug]/workspace/layout.tsx
@@ -6,6 +6,7 @@ import { authOptions } from '#/lib/auth';
 import { getTeams } from '#/lib/operations/teams/get-teams';
 import { hasTeamAccess } from '#/lib/operations/teams/team-authority';
 
+import { MobileNotSupported } from '#/components/dashboard/mobile-not-supported';
 import { NavBar } from '#/components/layout/navbar';
 import { Sidebar } from '#/components/layout/sidebar';
 import { SiteFooter } from '#/components/layout/site-footer';
@@ -39,7 +40,10 @@ export default async function WorkspaceSharedLayout({
       <NavBar user={user} teams={teams} activeTeamSlug={params.team_slug} />
       <div className="flex flex-1 flex-col ps-16 pt-16">
         <Sidebar links={links} />
-        <main className="flex w-full flex-1 flex-col">{children}</main>
+        <main className="flex w-full flex-1 flex-col">
+          <MobileNotSupported />
+          {children}
+        </main>
       </div>
       <SiteFooter />
     </div>
