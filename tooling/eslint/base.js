fc1b8e596698d6636257ba6f9cb25bcec7585061
Sam Rizvi <srizvi@users.noreply.github.com>
2024-01-21 19:14:50 -0800

    chore: ignore no unsafe return rule for now

--- tooling/eslint/base.js
+++ tooling/eslint/base.js
@@ -34,6 +34,7 @@ const config = {
     '@typescript-eslint/no-unsafe-argument': 'off',
     '@typescript-eslint/no-unsafe-call': 'off',
     '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
     '@typescript-eslint/no-unused-vars': [
       'error',
       {
